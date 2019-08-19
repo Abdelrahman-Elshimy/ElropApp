42884b76e12f35b27f2b9d74b39a8de6a954264e
= <=abdoelsimy123@gmail.com>
2019-08-20 01:40:05 +0200

    create new project and home page

---
+++ elropy/lib/screens/home.dart
@@ -0,0 +1,97 @@
+import 'package:elropy/utilities/colors.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  AppColors _appColors = AppColors();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          // create logo of elropy
+          Container(
+            margin: EdgeInsets.only(
+                left: MediaQuery.of(context).size.width * .2,
+                right: MediaQuery.of(context).size.width * .2,
+                top: 50),
+            padding: EdgeInsets.only(top: 30, bottom: 30),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              border: Border.all(
+                color: _appColors.mainColor,
+                width: 2,
+              ),
+            ),
+            child: Text(
+              'الروبي',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: _appColors.mainColor,
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+              ),
+            ),
+          ),
+
+          // button of customers
+
+          Container(
+            margin: EdgeInsets.only(left: MediaQuery.of(context).size.width * .1,
+                right: MediaQuery.of(context).size.width * .1, top: 30, bottom: 10),
+            child: RaisedButton(
+              color: _appColors.mainColor,
+              onPressed: (){},
+              padding: EdgeInsets.all(15),
+              child: Text('الزبائن', style: TextStyle(color: Colors.white,),),
+            ),
+          ),
+
+          // button of customers
+
+          Container(
+            margin: EdgeInsets.only(left: MediaQuery.of(context).size.width * .1,
+                right: MediaQuery.of(context).size.width * .1, top: 30, bottom: 10),
+            child: RaisedButton(
+              color: _appColors.mainColor,
+              onPressed: (){},
+              padding: EdgeInsets.all(15),
+              child: Text('المخزن', style: TextStyle(color: Colors.white,),),
+            ),
+          ),
+
+          // button of customers
+
+          Container(
+            margin: EdgeInsets.only(left: MediaQuery.of(context).size.width * .1,
+                right: MediaQuery.of(context).size.width * .1, top: 30, bottom: 10),
+            child: RaisedButton(
+              color: _appColors.mainColor,
+              onPressed: (){},
+              padding: EdgeInsets.all(15),
+              child: Text('ايرادات ومصروفات', style: TextStyle(color: Colors.white,),),
+            ),
+          ),
+
+                    // button of customers
+
+          Container(
+            margin: EdgeInsets.only(left: MediaQuery.of(context).size.width * .1,
+                right: MediaQuery.of(context).size.width * .1, top: 30, bottom: 10),
+            child: RaisedButton(
+              color: _appColors.mainColor,
+              onPressed: (){},
+              padding: EdgeInsets.all(15),
+              child: Text('التقارير', style: TextStyle(color: Colors.white,),),
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
---
+++ elropy/lib/utilities/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // main color of the project
+  Color mainColor = Colors.green;
+}
